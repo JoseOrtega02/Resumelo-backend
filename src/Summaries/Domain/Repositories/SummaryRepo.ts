bf363884a9ac5ffd4a022ca851599c38a81a12b0
jose <ortegajoseweb02@gmail.com>
2025-03-03 11:54:43 -0300

    fixing interface in summary repo

--- src/Summaries/Domain/Repositories/SummaryRepo.ts
+++ src/Summaries/Domain/Repositories/SummaryRepo.ts
@@ -3,7 +3,7 @@ import { ISummary } from "../Entities/ISummary";
 import { Summary } from "../Entities/Summary";
 
 export interface SummaryRepo{
-    create(summary: ISummary): Promise<ResultSet>;
+    create(summary: ISummary): Promise<ResultSet | void>;
     findById(id: string): Promise<Summary | null>;
     findAll(): Promise<Summary[]>; // Cambiado para devolver siempre un array
     put(summary: Summary,id:string): Promise<Summary | null>; // Se asume que `summary` ya contiene los datos actualizados
