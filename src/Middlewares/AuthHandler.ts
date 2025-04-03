b7a5ddd574b1355c7f377e4c4806ea8d553c3b5f
jose <ortegajoseweb02@gmail.com>
2025-04-03 10:01:13 -0300

    adding Auth

--- src/Middlewares/AuthHandler.ts
+++ src/Middlewares/AuthHandler.ts
@@ -1,15 +1,23 @@
 import { NextFunction, Request, Response } from "express";
-
-export const AuthHandler = async (
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config.env";
+export const AuthHandler = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token)
-    return res.status(401).json({ success: false, message: "No autorizado" });
   try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ success: false, message: "Unauthorized" });
+      return;
+    }
+    const decodedToken = jwt.verify(token, JWT_SECRET || "") as {
+      userId: string;
+    };
+    req.body.user = decodedToken;
+    next();
   } catch (error) {
-    next(error);
+    res.status(401).json({ success: false, message: "Invalid Token" });
   }
 };
