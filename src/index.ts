6c2b9265bba846ef697ba494ae077c97b15f27f3
jose <ortegajoseweb02@gmail.com>
2025-04-09 19:43:09 -0300

    adding the credentials option cors

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ import rateLimit from "express-rate-limit";
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "http://localhost:3000/",
+  origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
